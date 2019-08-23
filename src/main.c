69bcfa28a89449f63c7b70df8c021ec4f981e677
Maria Tatiane <tatiane-medeiros@users.noreply.github.com>
2019-08-23 10:49:03 -0300

    shell

--- src/main.c
+++ src/main.c
@@ -7,6 +7,19 @@
 #define SLEEP_TIME	500
 
 
+
+static int reset(const struct shell *shell, size_t argc, char **argv){
+	shell_print(shell, "Reset\n");
+	return 0;
+}
+
+SHELL_STATIC_SUBCMD_SET_CREATE(medidor_sub,
+	SHELL_CMD_ARG(reset, NULL, "Reset.", reset, 1, NULL),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
+);
+SHELL_CMD_REGISTER(medidor, &medidor_sub, "Comandos usando shell.", NULL);
+
+
 int main(void) {
     
     while(1){
